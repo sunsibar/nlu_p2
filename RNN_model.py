dfdd1c40aeee83a81cf42e161e230529cde324f6
su-si <susanne.h.keller@gmail.com>
2018-06-05 20:13:21 +0900

    added dropout

--- RNN_model.py
+++ RNN_model.py
@@ -5,6 +5,7 @@ class RNNModel():
     def __init__(self, rnn_config):
 
         self.config = rnn_config
+        self.reuse = rnn_config['mode'] == 'validate_RNN'
         self.embedding_dim = rnn_config['embedding_dim']
         #self.sequence_length = rnn_config['sequence_length']
         self.hidden_size = rnn_config['hidden_size']
@@ -17,13 +18,14 @@ class RNNModel():
         self.max_seq_length = tf.shape(self.input_x)[1]  # dynamic size
         self.sequence_mask = tf.sequence_mask(self.sequence_length_list, None, dtype=tf.float32)
 
-        self.word_embeddings = tf.get_variable("word_embeddings", [self.vocab_size, self.embedding_dim])
-        self.embedded_tokens = tf.nn.embedding_lookup(self.word_embeddings, self.input_x)
 
-        # split by the timestamp # Todo: Understand & try to remove sequence_length here --
-        # TODO: self.embedded_tokens = tf.unstack(self.embedded_tokens, num=self.max_seq_length, axis=1)# num=self.sequence_length, axis=1)
-        # rnncell = tf.nn.rnn_cell.BasicRNNCell(num_units=self.hidden_size)
-        with tf.variable_scope("rnn"):
+
+        with tf.variable_scope("rnn", reuse=self.reuse):
+            self.word_embeddings = tf.get_variable("word_embeddings", [self.vocab_size, self.embedding_dim])
+            self.embedded_tokens = tf.nn.embedding_lookup(self.word_embeddings, self.input_x)
+
+            # split by the timestamp #
+            # rnncell = tf.nn.rnn_cell.BasicRNNCell(num_units=self.hidden_size)
             if rnn_config['is_add_layer']:
                 rnncell = tf.nn.rnn_cell.LSTMCell(num_units=2*self.hidden_size)
                 W_middle = tf.get_variable("W_middle", shape=[2 * self.hidden_size, self.hidden_size],
@@ -33,39 +35,42 @@ class RNNModel():
             state = rnncell.zero_state(batch_size=self.batch_size, dtype=tf.float32)
             outputs, state = tf.nn.dynamic_rnn(rnncell, self.embedded_tokens, sequence_length=self.sequence_length_list,
                                                initial_state=state)
-            # TODO: test the above
 
-        if rnn_config['is_add_layer']:
-            outputs = tf.reshape(outputs,[-1,2*self.hidden_size])
-            self.outputs = tf.matmul(outputs,W_middle)
-        else:
-            self.outputs = tf.reshape(outputs,[-1,self.hidden_size])  # shape: (batch_size*time_step, self.hidden_size)
-        self.W_out = tf.get_variable("W_out", shape=[self.hidden_size, self.vocab_size],
-                                     initializer=tf.contrib.layers.xavier_initializer())
-        self.b_out = tf.Variable(tf.constant(0.1, shape=[self.vocab_size,]), name='b_out')
+            if rnn_config['is_add_layer']:
+                outputs = tf.reshape(outputs,[-1,2*self.hidden_size])
+                self.outputs = tf.matmul(outputs,W_middle)
+            else:
+                self.outputs = tf.reshape(outputs,[-1,self.hidden_size])  # shape: (batch_size*time_step, self.hidden_size)
+            self.W_out = tf.get_variable("W_out", shape=[self.hidden_size, self.vocab_size],
+                                         initializer=tf.contrib.layers.xavier_initializer())
+            self.b_out = tf.Variable(tf.constant(0.1, shape=[self.vocab_size,]), name='b_out')
 
-        logits = tf.nn.xw_plus_b(self.outputs, self.W_out, self.b_out)   # TODO: check the replacement works (of self.seq_length -> self.max_seq_length
-        logits = tf.reshape(logits, shape=[self.max_seq_length, -1, self.vocab_size])  # (time_step,batch_size,vocab_size)
-        self.logits = tf.transpose(logits, perm=[1, 0, 2])
-        self.prediction = tf.argmax(logits, 1, name='prediction')
-        self.loss = tf.contrib.seq2seq.sequence_loss(
-            self.logits,
-            self.input_y,
-            self.sequence_mask,
-            average_across_timesteps=True,
-            average_across_batch=False,name="loss")
+            # dropout
+            if rnn_config['mode'] == 'train_RNN':
+                self.outputs = tf.nn.dropout(self.outputs, keep_prob=0.4)
 
-        self.eva_perplexity = tf.exp(self.loss, name="eva_perplexity")
-        self.minimize_loss = tf.reduce_mean(self.loss,name="minize_loss")
-        self.print_perplexity = tf.reduce_mean(self.eva_perplexity, name="print_perplexity")
+            logits = tf.nn.xw_plus_b(self.outputs, self.W_out, self.b_out)
+            logits = tf.reshape(logits, shape=[self.max_seq_length, -1, self.vocab_size])  # (time_step,batch_size,vocab_size)
+            self.logits = tf.transpose(logits, perm=[1, 0, 2])
+            self.prediction = tf.argmax(logits, 1, name='prediction')
+            self.loss = tf.contrib.seq2seq.sequence_loss(
+                self.logits,
+                self.input_y,
+                self.sequence_mask,
+                average_across_timesteps=True,
+                average_across_batch=False,name="loss")
 
-        self.word_probabs = tf.exp(- tf.contrib.seq2seq.sequence_loss(
-            self.logits,
-            self.input_y,
-            self.sequence_mask,
-            average_across_timesteps=False,
-            average_across_batch=False,name="log_word_prob"))
-        self.sequence_probab = 1. / self.eva_perplexity
+            self.eva_perplexity = tf.exp(self.loss, name="eva_perplexity")
+            self.minimize_loss = tf.reduce_mean(self.loss,name="minize_loss")
+            self.print_perplexity = tf.reduce_mean(self.eva_perplexity, name="print_perplexity")
+
+            self.word_probabs = tf.exp(- tf.contrib.seq2seq.sequence_loss(
+                self.logits,
+                self.input_y,
+                self.sequence_mask,
+                average_across_timesteps=False,
+                average_across_batch=False,name="log_word_prob"))
+            self.sequence_probab = 1. / self.eva_perplexity
 
 
 
--- train_RNN.py
+++ train_RNN.py
@@ -17,7 +17,7 @@ from utils import id2word, load_data, build_dict, convert_text_data, get_batch_d
 from RNN_model import RNNModel
 from dataset import StoryDataset, storydata_from_csv, Preprocessor
 
-def train(model, rnn_config, train_dataset, val_dataset, id2word_dict):
+def train(model_train, model_val, rnn_config, train_dataset, val_dataset, id2word_dict):
     #assert val_dataset.data_size == val_dataset.feeder.batch_size
 
     learning_rate = rnn_config['learning_rate']
@@ -39,7 +39,7 @@ def train(model, rnn_config, train_dataset, val_dataset, id2word_dict):
     with sess.as_default():
         # define the training process
         tvars = tf.trainable_variables()
-        grads, _ = tf.clip_by_global_norm(tf.gradients(model.minimize_loss, tvars), max_grad_norm)
+        grads, _ = tf.clip_by_global_norm(tf.gradients(model_train.minimize_loss, tvars), max_grad_norm)
         optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
         global_step = tf.Variable(0, name="global_step", trainable=False)
         train_op = optimizer.apply_gradients(zip(grads, tvars),
@@ -56,7 +56,7 @@ def train(model, rnn_config, train_dataset, val_dataset, id2word_dict):
 
 
         # summary for the loss
-        loss_summary = tf.summary.scalar("print_perplexity", model.print_perplexity)
+        loss_summary = tf.summary.scalar("print_perplexity", model_train.print_perplexity)
 
         # train_summary
         train_summary_op = tf.summary.merge([loss_summary, grad_summaries_merged])
@@ -84,38 +84,46 @@ def train(model, rnn_config, train_dataset, val_dataset, id2word_dict):
                     my_embedding_matrix[id,:] = np.random.uniform(-0.25, 0.25,embedding_dim)
 
         word_embedding = tf.placeholder(tf.float32,[None,None], name="pretrained_embeddings")
-        set_x = model.word_embeddings.assign(word_embedding)
+        set_x = model_train.word_embeddings.assign(word_embedding)
+        set_x_val = model_val.word_embeddings.assign(word_embedding)
 
         sess.run(set_x, feed_dict={word_embedding:my_embedding_matrix})
+        sess.run(set_x_val, feed_dict={word_embedding:my_embedding_matrix})
 
-        epoch = 0
-        while epoch < num_epochs:
-            train_loss = 0
-            for ib, ibatch in enumerate(train_dataset.all_batches(shuffle=True)):
-                #X, Y, batch_seq_lengths = ibatch.get_padded_data()
-                feed_dict = model.get_feed_dict_train(ibatch)
-                _, summary, step, loss = sess.run([train_op, train_summary_op, global_step, model.print_perplexity],
-                                                    feed_dict=feed_dict)
-                train_summary_writer.add_summary(summary=summary, global_step=step)
-                print('\ribatch {:d}, max loss {:f}'.format(ib, np.max(loss)), end='')
-                train_loss += loss
-            # -- validate --
-            valid_loss = 0.
-            for ib, ibatch in enumerate(val_dataset.all_batches()):
-            #val_allbatch = val_dataset.next_batch(shuffle=False)
-                X_v, Y_v, val_seq_lengths = ibatch.get_padded_data()
-                valid_loss += sess.run([model.print_perplexity], feed_dict={model.input_x: X_v,
-                                                                       model.input_y: Y_v,
-                                                                       model.sequence_length_list: val_seq_lengths})[0]
-            print("\nep:", epoch, "train_loss", train_loss / train_dataset.data_size, flush=True)
-            print("ep:", epoch, "valid_loss", valid_loss, flush=True)
-            sys.stdout.flush()
-            if (epoch + 1) % rnn_config['save_checkpoints_every_epoch'] == 0:
-                ckpt_path = saver.save(sess, checkpoint_dir+"_ep"+str(epoch)+"/", global_step)
-                print('Model saved to file {}'.format(ckpt_path))
-            epoch += 1
-        ckpt_path = saver.save(sess,checkpoint_dir,global_step=step)
-        print("Stored trained model at: \n"+ckpt_path)
+
+        try:
+            epoch = 0
+            while epoch < num_epochs:
+                train_loss = 0
+                for ib, ibatch in enumerate(train_dataset.all_batches(shuffle=True)):
+                    #X, Y, batch_seq_lengths = ibatch.get_padded_data() # also todo: why are val and train loss so incredibly different?
+                    feed_dict = model_train.get_feed_dict_train(ibatch)  # todo: why different than below?
+                    _, summary, step, loss = sess.run([train_op, train_summary_op, global_step, model_train.print_perplexity],
+                                                        feed_dict=feed_dict)
+                    train_summary_writer.add_summary(summary=summary, global_step=step)
+                    print('\ribatch {:d}, max loss {:f}'.format(ib, np.max(loss)), end='')
+                    train_loss += loss
+                # -- validate --
+                valid_loss = 0.
+                for ib, ibatch in enumerate(val_dataset.all_batches()):
+                #val_allbatch = val_dataset.next_batch(shuffle=False)
+                    X_v, Y_v, val_seq_lengths = ibatch.get_padded_data()
+                    valid_loss += sess.run([model_val.print_perplexity], feed_dict={model_val.input_x: X_v,
+                                                                           model_val.input_y: Y_v,
+                                                                           model_val.sequence_length_list: val_seq_lengths})[0]
+                print("\nep:", epoch, "train_loss", train_loss / train_dataset.data_size, flush=True)
+                print("ep:", epoch, "valid_loss", valid_loss / val_dataset.data_size, flush=True)
+                sys.stdout.flush()
+                if (epoch + 1) % rnn_config['save_checkpoints_every_epoch'] == 0:
+                    ckpt_path = saver.save(sess, checkpoint_dir+"_ep"+str(epoch)+"/", global_step)
+                    print('Model saved to file {}'.format(ckpt_path))
+                epoch += 1
+        finally:
+            if not config['train_data_file'] == 'train_stories_sample.csv':
+                ckpt_path = saver.save(sess,checkpoint_dir,global_step=step)
+                print("Stored trained model at: \n"+ckpt_path)
+            else:
+                print("train data file indicates this is a debug run; not saving the model at shutdown.")
     return ckpt_path
 
 def evaluate(sess_path, eva_data, result_ptr):
@@ -240,9 +248,12 @@ def main(config):
     story_dataset_train.preprocess(prep)
     story_dataset_val.preprocess(prep)
 
-    model = RNNModel(config['rnn_config'])
+    model_train = RNNModel(config['rnn_config'])
+    val_rnn_config = config['rnn_config'].copy()
+    val_rnn_config['mode'] = 'validate_RNN'
+    model_val = RNNModel(val_rnn_config)
 
-    out_dir = train(model, config['rnn_config'], id2word_dict=prep.id2word_dict,
+    out_dir = train(model_train, model_val, config['rnn_config'], id2word_dict=prep.id2word_dict,
                     train_dataset=story_dataset_train, val_dataset=story_dataset_val)
 
 #    quiz_file = os.path.join(config['data_dir'], config['StoryCloze_file'])
