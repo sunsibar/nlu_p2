3975e62f39928f5a106607c99acc4cd07cdd4541
Susanne Keller <su-si@users.noreply.github.com>
2018-06-07 17:56:13 +0200

    Delete unneccesary things from config.py

--- config.py
+++ config.py
@@ -36,12 +36,6 @@ else:
 
 assert 'model_dir' not in rnn_config.keys() # will be written at train time and appended with a date
 
-# ---- toggle static features ---- #
-static_features = {}
-static_features['sentence_lengths'] = True
-static_features['sentiment'] = False # TODO
-# TODO: if you add new features, also add lines to the feature counting 'num_features(self)' in full_model.py
-
 # ---- Full model configuration parameters ---- #
 config = {}
 config['model_type'] = "simple"
@@ -55,19 +49,13 @@ config['max_grad_norm'] = 5
 rnn_config['save_checkpoints_every_epoch'] = 5
 rnn_config['n_checkpoints_to_keep'] = 2
 config['data_dir'] = '../data'
-config['output_dir'] = '../trained_models/full'
+#config['output_dir'] = '../trained_models/full'
 config['train_data_file'] = 'train_stories.csv'
-#config['train_data_file'] = 'train_stories_sample.csv'
 config['story_cloze_file'] = 'cloze_test_val__spring2016.csv'
 config['rnn_config'] = rnn_config
-config['static_features'] = static_features
-#config['max_sentence_length'] = 30 # in words, including special tokens and sentence endings ## TODO: unused so far...
 config['vocab_size'] = rnn_config['vocab_size'] = 20000
-#config['limit_num_samples'] = 20   # Unused ; None, or a positive number to reduce the number of samples during training
 
 # In training the final classifier, needs an RNN model that's already been trained
-config['rnn_model_dir'] = '../trained_models/RNN/simple-3L-100h_useE-True_addL-False/18-May-26_00h21-19' # add path to model here
-config['rnn_model_id'] = None # None, then use latest checkpoint, or add the checkpoint ID here
 config['name'] = config['model_type']
 if config['use_rnn']:
     config['name'] += "-" + rnn_config['model_type'] + "_rnn"
@@ -76,8 +64,3 @@ else:
 
 assert 'model_dir' not in config.keys() # will be created during training
 
-
-# For using an existing RNN:
-infer_config = config.copy()
-infer_config['model_dir'] = '../trained_models/full/myfancymodel' # add path to model here
-infer_config['model_id'] = None # None, then use latest checkpoint, or add the checkpoint ID here
